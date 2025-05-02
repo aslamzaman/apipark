7339a015d1a4a4c1a48f78f61d05b6f47459aedd
aslamcmes <aslamcmes@gmail.com>
2025-05-03 02:46:29 +0600

    popular movie added

---
+++ app/api/movie/popular/imdb/route.js
@@ -0,0 +1,31 @@
+import { sortArray } from '@/lib/utils';
+import { NextResponse } from 'next/server';
+
+
+export const GET = async (request) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_HOST_NAME}/movie/popular/popular_imdb.json`);
+        const jsonData = await res.json();
+   
+        const sortData = jsonData.sort((a, b)=>sortArray(b.rating, a.rating));
+        const lastToHunderd = sortData.slice(-100);
+     
+        const response = NextResponse.json(lastToHunderd);
+
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+        return response
+
+    } catch (error) {
+        console.log(error);
+        const response = NextResponse.json({
+            message: "An error occurred while fetching data.",
+            data: "",
+        }, { status: 500 });
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+        return response
+    }
+}
