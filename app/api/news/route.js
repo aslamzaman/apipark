b115e63cf7a056958ab168be8bf40b6e6c9fe43b
aslam <aslamcmes@gmail.com>
2025-05-15 16:40:16 +0600

    news added

---
+++ app/api/news/route.js
@@ -0,0 +1,36 @@
+import { getDataFromFirebase } from '@/lib/firebaseFunction';
+import { NextResponse } from 'next/server';
+
+
+export const GET = async (request) => {
+    try {
+        const data = await getDataFromFirebase("news");
+        const result = [];
+        for (let i = 0; i < data.length; i++) {
+            result.push({
+                title: data[i].title,
+                detail: data[i].detail,
+                poster: data[i].poster,
+                url: data[i].title,
+                date: data[i].date,
+                ref: data[i].ref
+            });
+        }
+
+        const response = NextResponse.json(result);
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+        return response
+    } catch (error) {
+        console.log(error);
+        const response = NextResponse.json({
+            message: "An error occurred while fetching data.",
+            data: "",
+        }, { status: 500 });
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+        return response
+    }
+}
